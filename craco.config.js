422ec1b09dbd0402c6eb027b6141ea6b82c3d76f
Aleksandr Osokin <osokinweb@gmail.com>
2022-11-22 17:05:36 +0300

    Remove craco-less, reinstall packages

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,6 @@
 const { resolve } = require('path')
-const CracoLessPlugin = require('craco-less')
 
 module.exports = {
-	plugins: [{ plugin: CracoLessPlugin }],
 	webpack: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
